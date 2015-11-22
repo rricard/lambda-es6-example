bccdaf2586350604d27c4fca9ae8a8ffe9375822
Robin Ricard <ricard.robin@gmail.com>
2015-11-22 20:11:19 +0100

    Setup module loaders
    Use babel for js files
    Use json for json files

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,21 @@
 var path = require("path");
 
 module.exports = {
-
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015'],
+          plugins: ['syntax-flow', 'transform-flow-strip-types']
+        }
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      }
+    ]
+  }
 };
