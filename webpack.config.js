8ee9824857f734d77a5cfa95b3fea6d1947a7e00
Robin Ricard <ricard.robin@gmail.com>
2015-11-22 20:48:33 +0100

    Webpack entry points
    Reads synchronously the lambda/ dir and create an entry point for each js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,19 @@
 var path = require("path");
+var fs = require("fs");
 
 module.exports = {
+  entry: fs.readdirSync(path.join(__dirname, "./lambdas"))
+         .filter(filename => /\.js$/.test(filename))
+         .map(filename => {
+           var entry = {};
+           entry[filename.replace(".js", "")] = path.join(
+             __dirname,
+             "./lambdas/",
+             filename
+           );
+           return entry;
+         })
+         .reduce((finalObject, entry) => Object.assign(finalObject, entry), {}),
   output: {
     path: path.join(__dirname, "dist"),
     library: "[name]",
