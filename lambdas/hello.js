860fd190f9aea4ce091f264824e87bab98e35cc5
Robin Ricard <ricard.robin@gmail.com>
2015-11-29 19:54:43 +0100

    Promisify lambdas

--- lambdas/hello.js
+++ lambdas/hello.js
@@ -1,9 +1,11 @@
 /* @flow */
 
+import type {LambdaContext} from "../lib/lambda-types.js";
+
 type HelloOptions = {
   name: string
 };
 
-export function hello(options: HelloOptions, context: any): void {
+export function hello(options: HelloOptions, context: LambdaContext): void {
   context.succeed(`Hello ${options.name || "world"}!`);
 }
